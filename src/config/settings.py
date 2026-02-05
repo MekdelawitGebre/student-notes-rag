87985fe7f31fed6dbab2ad921face5a893db665e
MekdelawitGebre <megdela21@gmail.com>
2026-02-05 20:23:34 +0300

    feat: add configuration settings for chunking, embeddings, LLM

---
+++ src/config/settings.py
@@ -0,0 +1,9 @@
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 100
+TOP_K = 3
+
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+LLM_MODEL_NAME = "google/flan-t5-base"
+
+MAX_TOKENS = 256
+TEMPERATURE = 0.2
